5a52ab430060c5e677ae66c1af6d81ac03157648
Aldarra Yazan (s158142) <yazan.aldarra@student.ap.be>
2026-06-07 14:23:00 +0200

    add: extend oil.nvim to fuzzy find to directory

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -27,6 +27,35 @@ return {
 
       -- Open parent directory in current window
       vim.keymap.set('n', '<leader>-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+      -- Fuzzy find directory and open in Oil
+      local function pick_directory()
+        local find_cmd
+        if vim.fn.executable 'fd' == 1 then
+          find_cmd = { 'fd', '--type', 'd', '--hidden', '--follow', '--exclude', '.git' }
+        elseif vim.fn.executable 'fdfind' == 1 then
+          find_cmd = { 'fdfind', '--type', 'd', '--hidden', '--follow', '--exclude', '.git' }
+        else
+          find_cmd = { 'find', '.', '-type', 'd', '-not', '-path', '*/.git/*' }
+        end
+
+        require('telescope.builtin').find_files {
+          find_command = find_cmd,
+          prompt_title = 'Open Directory in Oil',
+          attach_mappings = function(prompt_bufnr, map)
+            require('telescope.actions').select_default:replace(function()
+              local entry = require('telescope.actions.state').get_selected_entry()
+              require('telescope.actions').close(prompt_bufnr)
+              if entry and entry.path then
+                vim.cmd('Oil ' .. entry.path)
+              end
+            end)
+            return true
+          end,
+        }
+      end
+
+      vim.keymap.set('n', '<leader>sD', pick_directory, { desc = '[S]earch [D]irectory (Oil)' })
     end,
   },
 }
